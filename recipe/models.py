891a494417e4d3973792ee1744a3b4cc1c871e7f
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-07-11 12:16:26 +0800

    Included cdn BS, Boxicons, JQuery

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+
+       def fileName(instance, filename):
+              return '/'.join(['recipe', str(instance.name), filename])
+
+       recipe_name = models.CharField(max_length=200)
+       recipe_image = models.ImageField(upload_to=fileName, blank=True, null=True)
+       recipe_description = models.CharField(max_length=500)
+       recipe_ingredients = models.CharField(max_length=500)
+       date_created = models.DateTimeField(auto_now_add=True)
