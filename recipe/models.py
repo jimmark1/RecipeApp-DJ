eb31bc5ef859f8535df82f3bd57c7527030be651
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-07-12 11:02:49 +0800

    Data Rendering Availble

--- recipe/models.py
+++ recipe/models.py
@@ -3,10 +3,13 @@ from django.db import models
 class Recipe(models.Model):
 
        def fileName(instance, filename):
-              return '/'.join(['recipe', str(instance.name), filename])
+              return '/'.join(['recipe', str(instance.recipe_name), filename])
 
        recipe_name = models.CharField(max_length=200)
        recipe_image = models.ImageField(upload_to=fileName, blank=True, null=True)
        recipe_description = models.CharField(max_length=500)
        recipe_ingredients = models.CharField(max_length=500)
        date_created = models.DateTimeField(auto_now_add=True)
+
+       def __str__(self):
+              return self.recipe_name
--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 
+from . models import Recipe
+
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
+
+        qs = Recipe.objects.all()
+        context = {
+            'data' : qs
+        }
+
+        return render(request, 'home/index.html', context)
+    
+    def post(self, request):
+        pass
